c1985d1a93d38858483162c3d1006e53a3e032f0
nkout <nkoutsianas@gmail.com>
2024-10-20 22:43:02 +0300

    first change

--- shift_scheduling_sat.py
+++ shift_scheduling_sat.py
@@ -207,29 +207,29 @@ def add_soft_sum_constraint(
 def solve_shift_scheduling(params: str, output_proto: str):
     """Solves the shift scheduling problem."""
     # Data
-    num_employees = 8
-    num_weeks = 3
+    num_employees = 50
+    num_weeks = 4
     shifts = ["O", "M", "A", "N"]
 
     # Fixed assignment: (employee, shift, day).
     # This fixes the first 2 days of the schedule.
     fixed_assignments = [
-        (0, 0, 0),
-        (1, 0, 0),
-        (2, 1, 0),
-        (3, 1, 0),
-        (4, 2, 0),
-        (5, 2, 0),
-        (6, 2, 3),
-        (7, 3, 0),
-        (0, 1, 1),
-        (1, 1, 1),
-        (2, 2, 1),
-        (3, 2, 1),
-        (4, 2, 1),
-        (5, 0, 1),
-        (6, 0, 1),
-        (7, 3, 1),
+        (0, "O", 0),
+        (1, "O", 0),
+        (2, "M", 0),
+        (3, "M", 0),
+        (4, "A", 0),
+        (5, "A", 0),
+        (6, "A", 3),
+        (7, "N", 0),
+        (0, "M", 1),
+        (1, "M", 1),
+        (2, "A", 1),
+        (3, "A", 1),
+        (4, "A", 1),
+        (5, "O", 1),
+        (6, "O", 1),
+        (7, "N", 1),
     ]
 
     # Request: (employee, shift, day, weight)
@@ -237,12 +237,12 @@ def solve_shift_scheduling(params: str, output_proto: str):
     requests = [
         # Employee 3 does not want to work on the first Saturday (negative weight
         # for the Off shift).
-        (3, 0, 5, -2),
+        (3, "O", 5, -2),
         # Employee 5 wants a night shift on the second Thursday (negative weight).
-        (5, 3, 10, -2),
+        (5, "N", 10, -2),
         # Employee 2 does not want a night shift on the first Friday (positive
         # weight).
-        (2, 3, 4, 4),
+        (2, "N", 4, 4),
     ]
 
     # Shift constraints on continuous sequence :
@@ -250,10 +250,10 @@ def solve_shift_scheduling(params: str, output_proto: str):
     #             soft_max, hard_max, max_penalty)
     shift_constraints = [
         # One or two consecutive days of rest, this is a hard constraint.
-        (0, 1, 1, 0, 2, 2, 0),
+        ("O", 1, 1, 0, 2, 2, 0),
         # between 2 and 3 consecutive days of night shifts, 1 and 4 are
         # possible but penalized.
-        (3, 1, 2, 20, 3, 4, 5),
+        ("N", 1, 2, 20, 3, 4, 5),
     ]
 
     # Weekly sum constraints on shifts days:
@@ -261,34 +261,34 @@ def solve_shift_scheduling(params: str, output_proto: str):
     #             soft_max, hard_max, max_penalty)
     weekly_sum_constraints = [
         # Constraints on rests per week.
-        (0, 1, 2, 7, 2, 3, 4),
+        ("O", 1, 2, 7, 2, 3, 4),
         # At least 1 night shift per week (penalized). At most 4 (hard).
-        (3, 0, 1, 3, 4, 4, 0),
+        ("N", 0, 1, 3, 4, 4, 0),
     ]
 
     # Penalized transitions:
     #     (previous_shift, next_shift, penalty (0 means forbidden))
     penalized_transitions = [
         # Afternoon to night has a penalty of 4.
-        (2, 3, 4),
+        ("A", "N", 4),
         # Night to morning is forbidden.
-        (3, 1, 0),
+        ("N", "M", 0),
     ]
 
     # daily demands for work shifts (morning, afternon, night) for each day
     # of the week starting on Monday.
     weekly_cover_demands = [
-        (2, 3, 1),  # Monday
-        (2, 3, 1),  # Tuesday
-        (2, 2, 2),  # Wednesday
-        (2, 3, 1),  # Thursday
-        (2, 2, 2),  # Friday
-        (1, 2, 3),  # Saturday
-        (1, 3, 1),  # Sunday
+        {"M":2, "A":3, "N":1},  # Monday
+        {"M":2, "A":3, "N":1},  # Tuesday
+        {"M":2, "A":2, "N":2},  # Wednesday
+        {"M":2, "A":3, "N":1},  # Thursday
+        {"M":2, "A":2, "N":2},  # Friday
+        {"M":1, "A":2, "N":3},  # Saturday
+        {"M":1, "A":3, "N":1},  # Sunday
     ]
 
     # Penalty for exceeding the cover constraint per shift type.
-    excess_cover_penalties = (2, 2, 5)
+    excess_cover_penalties = {"M": 2, "A":2, "N":5}
 
     num_days = num_weeks * 7
     num_shifts = len(shifts)
@@ -314,18 +314,18 @@ def solve_shift_scheduling(params: str, output_proto: str):
 
     # Fixed assignments.
     for e, s, d in fixed_assignments:
-        model.add(work[e, s, d] == 1)
+        model.add(work[e, shifts.index(s), d] == 1)
 
     # Employee requests
     for e, s, d, w in requests:
-        obj_bool_vars.append(work[e, s, d])
+        obj_bool_vars.append(work[e, shifts.index(s), d])
         obj_bool_coeffs.append(w)
 
     # Shift constraints
     for ct in shift_constraints:
         shift, hard_min, soft_min, min_cost, soft_max, hard_max, max_cost = ct
         for e in range(num_employees):
-            works = [work[e, shift, d] for d in range(num_days)]
+            works = [work[e, shifts.index(shift), d] for d in range(num_days)]
             variables, coeffs = add_soft_sequence_constraint(
                 model,
                 works,
@@ -345,7 +345,7 @@ def solve_shift_scheduling(params: str, output_proto: str):
         shift, hard_min, soft_min, min_cost, soft_max, hard_max, max_cost = ct
         for e in range(num_employees):
             for w in range(num_weeks):
-                works = [work[e, shift, d + w * 7] for d in range(7)]
+                works = [work[e, shifts.index(shift), d + w * 7] for d in range(7)]
                 variables, coeffs = add_soft_sum_constraint(
                     model,
                     works,
@@ -365,8 +365,8 @@ def solve_shift_scheduling(params: str, output_proto: str):
         for e in range(num_employees):
             for d in range(num_days - 1):
                 transition = [
-                    ~work[e, previous_shift, d],
-                    ~work[e, next_shift, d + 1],
+                    ~work[e, shifts.index(previous_shift), d],
+                    ~work[e, shifts.index(next_shift), d + 1],
                 ]
                 if cost == 0:
                     model.add_bool_or(transition)
@@ -380,15 +380,20 @@ def solve_shift_scheduling(params: str, output_proto: str):
                     obj_bool_coeffs.append(cost)
 
     # Cover constraints
-    for s in range(1, num_shifts):
+    for s in shifts:
         for w in range(num_weeks):
             for d in range(7):
-                works = [work[e, s, w * 7 + d] for e in range(num_employees)]
+                works = [work[e, shifts.index(s), w * 7 + d] for e in range(num_employees)]
                 # Ignore Off shift.
-                min_demand = weekly_cover_demands[d][s - 1]
+                if s not in weekly_cover_demands[d]:
+                    continue
+                min_demand = weekly_cover_demands[d][s]
                 worked = model.new_int_var(min_demand, num_employees, "")
                 model.add(worked == sum(works))
-                over_penalty = excess_cover_penalties[s - 1]
+
+                if s not in excess_cover_penalties:
+                    continue
+                over_penalty = excess_cover_penalties[s]
                 if over_penalty > 0:
                     name = f"excess_demand(shift={s}, week={w}, day={d})"
                     excess = model.new_int_var(0, num_employees - min_demand, name)
